78720d67f2a914f3e850397fbec078ef4e8e2881
Marcus Chevalier <CueBall@Marcuss-MacBook-Pro.local>
2017-06-05 11:19:32 -0500

    React, Main Boilerplate

---
+++ main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Loop, Stage } from 'react-game-kit';
+
+
+class Game extends Component {
+    render() {
+  return (
+    <Loop>
+      <Stage>
+        // Game specific components go here
+      </Stage>
+    </Loop>
+  );
+            }
+}
